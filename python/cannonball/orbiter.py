886c159cd7860f896ed4ae6f23aa2a1a82c8d585
John Woods <john.o.woods@gmail.com>
2018-04-28 16:55:12 +0930

    Added orbital element computation using SPICE. Added an orbital elements functions and logger.

--- python/cannonball/orbiter.py
+++ python/cannonball/orbiter.py
@@ -3,6 +3,10 @@ import rk4
 from log import Log
 
 import gravity
+import spiceypy as spice
+
+import numpy as np
+from scipy.linalg import norm
 
 def make_elements(r_periapsis  = None,
                   eccentricity = None,
@@ -19,15 +23,36 @@ class Orbiter(object):
                  trv          = None,
                  elements     = None,
                  epoch_time   = 0.0,
-                 grav_bodies  = []):
-        if elements is not None:
-            import spiceypy as spice
-            trv = np.hstack((0.0, spice.conics(elements, epoch_time)))
-        
-        self.trv         = trv
-        self.log         = Log('trv.log', mode = 'w')
+                 conic_mu     = 3.986004415e14,
+                 use_logs     = {'inertial': 'inertial.log',
+                                 'elements': 'elements.log'}):
+        """ Constructor. Must provide either trv or elements/epoch_time.
+        Generally expects everything to be in meters.
 
-        self.log.write(trv)
+        Args:
+            trv:        initial time, position, velocity vector
+            elements:   orbital elements, e.g. from make_elements()
+            epoch_time: epoch time of elements (default: 0.0)
+            conic_mu:   gravitational constant for central body (e.g. earth)
+                        for conic approximation (default is earth); should be
+                        in m^3/s^2"""
+        self.conic_mu    = conic_mu
+        
+        if elements is not None:
+            # Note that SPICE uses km, km/s, and we use m, m/s
+            trv = np.hstack((0.0, spice.conics(elements, epoch_time) * 1000.0))
+        
+        self.trv          = trv
+
+        self.logs         = {}
+        if use_logs['inertial']:
+            self.logs['inertial'] = Log(use_logs['inertial'], mode = 'w')
+            self.log_inertial()
+        if use_logs['elements']:
+            self.logs['elements'] = Log(use_logs['elements'], mode = 'w')
+            self.log_elements()
+        
+        
 
     def r_INERTIAL(self):
         return self.trv[1:4]
@@ -42,17 +67,75 @@ class Orbiter(object):
 
         while self.trv[0] < t_max:
             dt, trv, temp = rk4.adaptive_step(self.trv, dt, gravity.basic_gravity, accuracy, step_using_index = False, **step_kwargs)
-            print "trv time is ", trv[0]
             dt_min = min(dt, dt_min)
             dt_max = max(dt, dt_min)
 
             if trv[0] > t_max: # Try again with a smaller step
-                print "trying again with smaller step from ", self.trv[0], " to ", t_max
-                trv, temp = rk4.step(self.trv, t_max - self.trv[0], gravity.basic_gravity, step_using_index = False, **step_kwargs)
+                trv, temp = rk4.step(self.trv, t_max - self.trv[0], gravity.basic_gravity, step_using_index = False, mu = self.conic_mu, **step_kwargs)
                 self.trv = trv
             else:
                 self.trv = trv
 
-            self.log.write(trv)
+            if 'inertial' in self.logs: self.log_inertial()
+            if 'elements' in self.logs: self.log_elements()
 
         return
+
+    def log_inertial(self):
+        return self.logs['inertial'].write(self.trv)
+
+    def log_elements(self):
+        return self.logs['elements'].write(np.hstack((self.trv[0], self.conic_elements()[0:6])))
+
+    def conic_elements(self):
+        """Compute the conic elements of the orbiter at the current
+        time. Uses SPICE.
+        """
+        time     = self.trv[0]
+        rv_km    = self.trv[1:] / 1000.0 # convert to km, km/sec
+        elements = spice.oscelt(rv_km, time, self.conic_mu * 1e-9)
+        elements[0] *= 1000.0 # convert perifocal distance to meters
+
+        return elements
+
+    def r_periapsis(self):
+        return self.conic_elements()[0]
+
+    def r_apoapsis(self):
+        # rp = a * (1 - e) => a = rp / (1 - e)
+        # ra = a * (1 + e) => ra = rp * (1 + e) / (1 - e)
+        elements = self.conic_elements()
+        rp = elements[0]
+        e  = elements[1]
+        return rp * (1.0 + e) / (1.0 - e)
+
+    def specific_energy(self):
+        v2    = self.trv[4:7].dot(self.trv[4:7])
+        rmag  = norm(self.trv[1:4])
+        return v2 * 0.5 - self.conic_mu / rmag
+    
+    def ecc(self):
+        return self.conic_elements()[1]
+
+    def inc(self):
+        return self.conic_elements()[2]
+
+    def lnode(self):
+        """Longitude of the ascending node"""
+        return self.conic_elements()[3]
+
+    def argp(self):
+        """Argument of periapsis"""
+        return self.conic_elements()[4]
+
+    def mean_anomaly_at_epoch(self):
+        return self.conic_elements()[5]
+
+    def semimajor_axis(self):
+        # FIXME: This is elliptical only; for hyperbola, should be
+        # negative.
+        elements = self.conic_elements()
+        rp = elements[0]
+        e  = elements[1]
+        a  = rp / (1.0 - e)
+        return rp / (1.0 - e)
--- python/test/test_orbiter.py
+++ python/test/test_orbiter.py
@@ -3,28 +3,108 @@ from .context import *
 from cannonball import Orbiter
 from cannonball.log import Log
 
+from scipy.linalg import norm
+
+PLOT = True
+
 class TestOrbiter(unittest.TestCase):
 
     def setUp(self):
         self.trv = np.array([0.0,
                             7000000.0, 0.0,    0.0,
-                            0.0,       4000.0, 0.0])
+                            0.0,       6500.0, 0.0])
+
+    def test_conic_elements(self):
+        trv = self.trv
+        orb = Orbiter(trv)
+        elements = orb.conic_elements()
         
-    def test_integrate_log(self):
+    def test_integrate_log_and_elements(self):
         """Integrates, stores, and reads a trajectory"""
+        # Also, plots trajectory and orbital elements for visual
+        # verification.
+        
         trv  = self.trv
-        max_time = 20.0
+        max_time = 500 * 60.0
         dt_guess = 0.5
 
         orb  = Orbiter(trv)
         orb.integrate(dt_guess, max_time)
-        orb.log.close()
+        for log in orb.logs:
+            orb.logs[log].close()
         
-        log  = Log('trv.log')
-        data = log.read()
-        np.testing.assert_equal(data[0,0], 0.0)
-        np.testing.assert_equal(data[0,1], dt_guess)
-        np.testing.assert_equal(data[0,-1], max_time)
+        inrtl_log  = Log('inertial.log')
+        inrtl = inrtl_log.read()
+        np.testing.assert_equal(inrtl[0,0], 0.0)
+        np.testing.assert_equal(inrtl[0,1], dt_guess)
+        np.testing.assert_equal(inrtl[0,-1], max_time)
+
+        elts_log = Log('elements.log')
+        elts = elts_log.read()
+
+        import pdb
+        pdb.set_trace()
+        
+        if PLOT:
+            import matplotlib.pyplot as plt
+            fig = plt.figure()
+            ax = fig.add_subplot(321)
+            ax.set_title("Inertial position over time")
+            ax.plot(inrtl[0,:], inrtl[1,:])
+            ax.plot(inrtl[0,:], inrtl[2,:])
+            ax.plot(inrtl[0,:], inrtl[3,:])
+            ax.set_ylabel("meters")
+            ax.grid(True)
+            plt.setp(ax.get_xticklabels(), visible=False)
+            ax0 = ax
+
+            ax = fig.add_subplot(322, sharex=ax0, sharey=ax0)
+            ax.set_title("Periapsis and apoapsis over time")
+            ax.plot(elts[0,:], elts[1,:])
+            ax.plot(elts[0,:], elts[1,:] * (elts[2,:] + 1.0) / (1.0 - elts[2,:]))
+            ax.set_ylabel("meters")
+            ax.grid(True)
+            plt.setp(ax.get_xticklabels(), visible=False)            
+            
+            ax = fig.add_subplot(323, sharex=ax0)
+            ax.set_title("Inertial velocity over time")
+            ax.plot(inrtl[0,:], inrtl[4,:])
+            ax.plot(inrtl[0,:], inrtl[5,:])
+            ax.plot(inrtl[0,:], inrtl[6,:])
+            ax.set_ylabel("meters/second")
+            ax.grid(True)
+            plt.setp(ax.get_xticklabels(), visible=False)
+
+            ax = fig.add_subplot(324, sharex=ax0)
+            ax.set_title("Element angles over time")
+            ax.plot(elts[0,:], elts[3,:] * (180.0 / math.pi), label='inc')
+            ax.plot(elts[0,:], elts[4,:] * (180.0 / math.pi), label='lnode')
+            ax.plot(elts[0,:], elts[5,:] * (180.0 / math.pi), label='argp')
+            ax.plot(elts[0,:], elts[6,:] * (180.0 / math.pi), label='m0')
+            #ax.set_xlabel("Time (s)")
+            ax.set_ylabel("degrees")
+            ax.grid(True)
+
+            ax = fig.add_subplot(325, sharex=ax0)
+            ax.set_title("Eccentricity over time")
+            ax.plot(elts[0,:], elts[2,:])
+            ax.set_xlabel("Time (s)")
+            ax.set_ylabel("eccentricity")
+            
+            # The above looks like it's increasing over time. I'm not
+            # worried about it.  I think it's increasing because of
+            # the adaptive step size in the rk4 integrator creating a
+            # phase shift with a really long period.  You can examine
+            # this effect if you zoom in on the top points in this
+            # next plot:
+
+            ax = fig.add_subplot(326, sharex=ax0)
+            ax.set_title("Radius")
+            ax.plot(inrtl[0,:], norm(inrtl[1:4,:], axis=0))
+            ax.set_ylabel("meters")
+            ax.set_xlabel("Time (s)")
+
+            plt.show()
 
 
 if __name__ == '__main__':
